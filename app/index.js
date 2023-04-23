e452afb2608f7fa0c146c42dc76e4e58222cbed9
Tarik ID BELLOUCH <tarikidb.dev@gmail.com>
2023-04-23 15:36:20 +0200

    Adding routes

--- app/index.js
+++ app/index.js
@@ -1,5 +1,23 @@
 import { View, Text } from "react-native";
+import { useRouter } from "expo-router";
 
-export default function Home() {
-  return <View></View>;
+export default function Page() {
+  const router = useRouter();
+
+  return (
+    <View>
+      <Text
+        style={{
+          backgroundColor: "red",
+          margin: 100,
+          height: 100,
+        }}
+        onPress={() => {
+          router.push("/search");
+        }}
+      >
+        Settings
+      </Text>
+    </View>
+  );
 }
